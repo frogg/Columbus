8bb3015c82276cd305cc3e00428348ef468e28c6
Frederik <f@polarapps.de>
2014-10-31 18:09:53 +0100

    added Xcode Project (iOS Folder)

---
+++ iOS/Hackathon Stuttgart 2014/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Hackathon Stuttgart 2014
+//
+//  Created by Frederik Riedel on 31.10.14.
+//  Copyright (c) 2014 Frederik Riedel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
