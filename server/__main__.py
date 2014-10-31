d59ca91ef5d6a040706c7065101f632af6b91a76
Timo Müssig <erasio@erasio.de>
2014-10-31 23:32:54 +0100

    changed stuff

--- server/__main__.py
+++ server/__main__.py
@@ -8,12 +8,13 @@ def hi():
     return "hey"
 
 class pushNotificationResponse():
-    def __init__(self, name, gps, typ, distance, schlagworte, requestid):
+    def __init__(self, name, gps, typ, distance, schlagworte, requestid, pageid):
         self.name = name
         self.gps = gps
         self.typ = typ
         self.schlagworte = schlagworte
         self.requestid = requestid
+        self.pageid = pageid
 
     def toDict(self):
         json = {
@@ -25,13 +26,45 @@ class pushNotificationResponse():
         }
         return json
 
-def geosearch(latitude, longtitude, format):
-    r = requests.get("https://en.wikipedia.org/w/api.php?action=query&format=json&gsprop=type&type=landmark&list=geosearch&gsradius=10000&gscoord=37.786971|-122.399677")
+def geosearch(latitude, longtitude, gtype,radius):
+    return apicall('de','query','json','geosearch',"&type={0}&gsradius={3}&gscoord={1}|{2}".format(gtype, latitude,longtitude,radius))
+
+
+def apicall(language, action, response_format, listParams, specialValues):
+    r = requests.get("https://{0}.wikipedia.org/w/api.php?action={1}&format={2}&list={3}{4}".format(language,action,response_format,listParams,specialValues))
     r = r.json().get('query').get('geosearch')
     return r
 
+def getSchlagworter(content):
+    r = requests.get('https://api.idolondemand.com/1/api/sync/extractconcepts/v1?text={0}&apikey=f0438796-6744-4ca2-9923-34605b45b713'.format(content))
+    if not r.status_code == requests.codes.ok:
+        return []
+    z = r.json()
+    keywords= []
+    for word in z['concepts'][:5]:
+        keywords.append(word['concept'])
+    return keywords
+
 @app.route('/get/locations/pushNotification/<latitude>/<longtitude>')
 def getPushLocations(latitude, longtitude):
+    locations = []
+    for i in geosearch(latitude, longtitude, 'landmark',1000):
+        if len(locations)>5:
+            break
+        entry =pushNotificationResponse(i.get('title'),
+                                        {'lat':i.get('lat'),
+                                         'lon':i.get('lon')}, 
+                                         "typ", 
+                                         i.get('dist'), 
+                                         "test",
+                                         "stuff",
+                                         i.get('pageid'))
+
+        locations.append(entry.toDict())
+    return jsonify({'notes':locations}), 200
+
+@app.route('/get/locations/<latitude>/<longtitude>')
+def pushLocations(latitude, longtitude):
     locations = []
     for i in geosearch(latitude, longtitude, 'json'):
         if len(locations)>5:
@@ -41,15 +74,12 @@ def getPushLocations(latitude, longtitude):
                                          'long':i.get('long')}, 
                                          "typ", 
                                          i.get('dist'), 
+                                         getSchlagworter(wikipedia.summary(i.get('title'))),
                                          "stuff",
-                                         "stuff")
+                                         i.get('pageid'))
+
         locations.append(entry.toDict())
     return jsonify({'notes':locations}), 200
-
-@app.route('/get/locations/<latitude>/<longtitude>')
-def pushLocations(latitude, longtitude):
-    return wikipedia.geosearch(latitude, longtitude)
-    return 'Hello World!'
 '''
 @app.route('/get/userID')
 def pushLocations(latitude, longtitude):
--- server/test.py
+++ server/test.py
@@ -3,14 +3,11 @@ import json
 import wikipedia
 
 def getSchlagworter(content):
-    r = requests.get('https://api.idolondemand.com/1/api/sync/extractconcepts/v1?text='+content+'&apikey=f0438796-6744-4ca2-9923-34605b45b713')
-    # print(r.status_code)
+    r = requests.get('https://api.idolondemand.com/1/api/sync/extractconcepts/v1?text={0}&apikey=f0438796-6744-4ca2-9923-34605b45b713'.format(content))
+    if not r.status_code == requests.codes.ok:
+    	return []
     z = r.json()
-    # z.get('concepts')
-    for i in z['concepts']:
-        print(i['concept'])
-
-    print("sdfsad")
-    return "sdfsad"
+        
+    return z['concepts'].fetch(10)
 
 getSchlagworter('Das Mercedes-Benz-Museum befand sich bis zum 18. März 2006 auf dem Gelände des Mercedes-Benz-Werkes')
