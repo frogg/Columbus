6e9c90d957ed4508777763afdda6f681e5445a6c
Frederik <f@polarapps.de>
2014-10-31 18:23:37 +0100

    iOS project is now named "Columbus"

---
+++ iOS/Columbus/MainViewController.m
@@ -0,0 +1,39 @@
+//
+//  MainViewController.m
+//  Hackathon Stuttgart 2014
+//
+//  Created by Frederik Riedel on 31.10.14.
+//  Copyright (c) 2014 Frederik Riedel. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title=@"Titel unserer App";
+    self.view.backgroundColor=[UIColor whiteColor];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
